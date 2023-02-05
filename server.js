111801d5e1896555a0edb39a73d0427eb3fb838b
mdpais <mdpais@gmail.com>
2023-02-05 12:52:30 -0400

    added code to switch statements

--- server.js
+++ server.js
@@ -19,12 +19,6 @@ const db = mysql.createConnection(
   console.log(`Connected to the employee_tracker database.`)
 );
 
-db.query(
-  'SELECT * FROM employee WHERE manager_id = 2',
-  function(err, results) {
-  }
-);
-
 const questions = [
   {
       type: `list`,
@@ -41,8 +35,14 @@ const questions = [
   {
       type: `input`,
       name: `add_role`,
-      message: `What is the name of the department that you want to add?`,
+      message: `What is the name of the role that you want to add?`,
       when: (answers) => answers.options === `Add a role`
+  },
+  {
+    type: `input`,
+    name: `add_role_salary`,
+    message: `What is the salary for the role?`,
+    when: (answers) => answers.options === `Add a role`
   },
   {
     type: `input`,
@@ -75,13 +75,13 @@ const questions = [
   },
   {
     type: `input`,
-    name: `update_employee_role1`,
+    name: `update_employee_id`,
     message: `What's the ID of employee to be updated?`,
     when: (answers) => answers.options === `Update employee role`
   },
   {
     type: `input`,
-    name: `update_employee_role2`,
+    name: `update_employee_roleid`,
     message: `What's the ID of the new role to be assigned to the employee?`,
     when: (answers) => answers.options === `Update employee role`
   }];
@@ -90,29 +90,77 @@ const questions = [
     inquirer.prompt(questions).then((answers) => {
         switch(answers.options){
           case `View all departments`:
-
+            db.query(
+              `SELECT * FROM department`,
+              function(err, results) {
+                console.table(results);
+                init();
+                return;
+              }
+            );
             break;
           case `View all roles`:
-              
+            db.query(
+              `SELECT * FROM role`,
+              function(err, results) {
+                console.table(results);
+                init();
+                return;
+              }
+            );
             break;
           case `View all employees`:
-            
+            db.query(
+              `SELECT * FROM employee`,
+              function(err, results) {
+                console.table(results);
+                init();
+                return;
+              }
+            );
             break;
           case `Add a department`:
-            
+            db.query(
+              `INSERT INTO department (name) VALUES ("${answers.add_department}");`,
+              function(err, results) {
+                console.log(`${answers.add_department} added successfully into departments.`);
+                init();
+                return;
+              }
+            );
             break;
           case `Add a role`:
-              
+            db.query(
+              `INSERT INTO role (title, salary, department_id) VALUES ("${answers.add_role}", ${answers.add_role_salary}, ${answers.add_role_deptid});`,
+              function(err, results) {
+                console.log(`${answers.add_role} added successfully into roles.`);
+                init();
+                return;
+              }
+            );
             break;
           case `Add an employee`:
-            
+            db.query(
+              `INSERT INTO employee (title, salary, department_id) VALUES ("${answers.add_role}", ${answers.add_role_salary}, ${answers.add_role_deptid});`,
+              function(err, results) {
+                console.log(`${answers.add_employee_first} ${answers.add_employee_last} added successfully into employees.`);
+                init();
+                return;
+              }
+            );
             break;
           case `Update employee role`:
-            
+            db.query(
+              `UPDATE employee SET role_id=${answers.update_employee_roleid} WHERE id=${answers.update_employee_id};`,
+              function(err, results) {
+                console.log(`Role ID for employee ID ${answers.update_employee_id} updated to ${answers.update_employee_roleid}.`);
+                init();
+                return;
+              }
+            );
             break;
           case `Quit`:
-
-            break;
+            return;
         };
       });
 }
